909cac909668556fcdc6a5557398168f06338341
Frederick Javalera <fjavalera@goldpointsystems.com>
2021-03-24 10:30:40 -0600

    Init commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by GPS Dev on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookReader
+//
+//  Created by GPS Dev on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
